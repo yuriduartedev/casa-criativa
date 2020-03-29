13258f52bfb04746c816fdae5aa472429ad5176f
Yuri Duarte <yuriduartedev@gmail.com>
2020-03-29 02:21:47 -0300

    Faz integração do fomulário com o banco de dados.

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,27 @@ function onOff() {
     .classList
     .toggle('addScroll');
 }
+
+function checkFields(event)  {
+  const valuesToChek = [
+    "title",
+    "category",
+    "image",
+    "description",
+    "link",
+  ]
+
+  const isEmpty = valuesToChek.find(function (value) {
+    const checkIsString = typeof event.target[value].value === "string"
+    const checkIfIsEmpty = !event.target[value].value.trim();
+    if (checkIsString && checkIfIsEmpty) {
+      return true;
+    }
+  });
+
+  if(isEmpty) {
+    event.preventDefault();
+    alert('Por favor preencha todos os campos!')
+  }
+
+}
--- server.js
+++ server.js
@@ -1,54 +1,13 @@
 // Usei o express para criar e configurar meu servidor
 const express = require('express');
 const server = express();
-
-const ideas = [
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    title: "Cursos de Programação",
-    category: "Estudo",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro. Laboriosam, quam! Accusamus, modi.",
-    url: "http://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-    title: "Exercícios",
-    category: "Saúde",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro. Laboriosam, quam! Accusamus, modi.",
-    url: "http://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-    title: "Meditação",
-    category: "Mentalidade",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro. Laboriosam, quam! Accusamus, modi.",
-    url: "http://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-    title: "Karaokê",
-    category: "Diversão em família",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro. Laboriosam, quam! Accusamus, modi.",
-    url: "http://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-    title: "Pintura",
-    category: "Criatividade",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro. Laboriosam, quam! Accusamus, modi.",
-    url: "http://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-    title: "Recortes",
-    category: "Criatividade",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro. Laboriosam, quam! Accusamus, modi.",
-    url: "http://rocketseat.com.br"
-  },
-]
+const db = require('./db');
 
 // configurar arquivos estáticos (css, scripts, images)
 server.use(express.static("public"));
+
+// habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 // configuração do template engineer nunjucks
 const nunjucks = require("nunjucks");
@@ -59,26 +18,68 @@ nunjucks.configure("views", {
 
 // Criei uma rota e capturo o pedido do cliente para responder
 server.get("/", function (req, res) {
-
-  const reversedIdeas = [...ideas].reverse();
-
-  let lastIdeas = []
-  for (idea of reversedIdeas) {
-    if (lastIdeas.length < 2) {
-      lastIdeas.push(idea)
+  db.all(`SELECT * FROM ideas`, function (err, rows) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro no banco de dados!");
     }
-  }
 
-  return res.render("index.html", { ideas: lastIdeas });
+    const reversedIdeas = [...rows].reverse();
+
+    let lastIdeas = []
+    for (idea of reversedIdeas) {
+      if (lastIdeas.length < 2) {
+        lastIdeas.push(idea)
+      }
+    }
+
+    return res.render("index.html", { ideas: lastIdeas });
+  });
 });
 
 server.get("/ideias", function (req, res) {
+  db.all(`SELECT * FROM ideas`, function (err, rows) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro no banco de dados!");
+    }
 
-  const reversedIdeas = [...ideas].reverse();
+    const reversedIdeas = [...rows].reverse();
 
-  return res.render("ideias.html", { ideas: reversedIdeas });
+    return res.render("ideias.html", { ideas: reversedIdeas });
+  })
+
 });
 
+server.post("/", function (req, res) {
+  // Inserir dados na tabela
+  const query = `
+    INSERT INTO ideas(
+      image,
+      title,
+      category,
+      description,
+      link
+    ) VALUES (?,?,?,?,?);
+  `
+
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ]
+
+  db.run(query, values, function (err) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro no banco de dados!");
+    }
+
+    return res.redirect("/ideias");
+  });
+});
 
 // Liguei meu servidor na porta 3000
 server.listen(3000);
